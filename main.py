1c4b65dfaab4edfa478cfb93cf2023ad9d7d9b9c
heww <he.wei@email.cn>
2023-11-02 18:02:20 +0800

    - [refactor] 不堵塞主线程

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+import queue
+import sys
+from concurrent.futures import ThreadPoolExecutor
+from threading import get_ident
 from tkinter import *
 from tkinter import filedialog, messagebox
 from tkinter.ttk import Combobox
@@ -8,107 +12,169 @@ import yaml
 from script import audio_tool, whisper_tool
 
 
-with open('config.yaml', encoding='utf-8') as f:
-    config = yaml.load(f.read(), Loader=yaml.FullLoader)
+class Text_Queue(queue.Queue):
+    def __init__(self, maxsize: int = 0) -> None:
+        super().__init__(maxsize)
+
+    def write(self, content):
+        self.put(content)
+
+    def flush(self):
+        pass
 
 
-def generate_subtitle(input, lang='en', model_size='base'):
+class App():
 
-    print('input='+input)
-    print('lang='+lang)
+    def __init__(self) -> None:
 
-    last_dot_index = input.rfind('.')
-    mp3_output = input[:last_dot_index]+'.mp3'
-    srt_output = input[:last_dot_index]+'.srt'
+        self._thread_pool = ThreadPoolExecutor(os.cpu_count())
 
-    print('audio extract begin')
-    audio_tool.audio_extract(input, mp3_output)
-    print('audio extract success')
+        self.window = Tk()
+        ws = self.window.winfo_screenwidth()
+        hs = self.window.winfo_screenheight()
+        w = 300
+        h = 500
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+        self.window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.window.resizable(False, False)
+        self.window.title('Extract subtitle')
 
-    print('whisper begin')
-    whisper_tool.do_whisper(audio=mp3_output, srt_path=srt_output, language=lang,
-                            model_size=model_size,
-                            model_download_root=config['model_download_root'])
-    print('whisper success')
+        model_frame = Frame(self.window)
+        model_frame.pack(fill='both', pady=10)
+        model_lbl = Label(model_frame, text='MODEL', width=10)
+        model_lbl.pack(side=LEFT)
+        self.model_combo = Combobox(model_frame, state='readonly')
+        self.model_combo['values'] = ('tiny.en', 'tiny', 'base.en', 'base', 'small.en',
+                                      'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large')
+        self.model_combo.current(3)
+        self.model_combo.pack(side=LEFT, ipadx=26)
 
-    print('remove temporary file')
-    os.remove(mp3_output)
+        lang_frame = Frame(self.window)
+        lang_frame.pack(fill='both', pady=10)
+        lang_lbl = Label(lang_frame, text='LANGUAGE', width=10)
+        lang_lbl.pack(side=LEFT)
+        self.lang_combo = Combobox(lang_frame, state='readonly')
+        self.lang_combo['values'] = ('en', 'ja', 'zh')
+        self.lang_combo.current(0)
+        self.lang_combo.pack(side=LEFT, ipadx=26)
 
-    print('success')
+        def choose_file():
+            files = filedialog.askopenfilenames(
+                filetypes=[('Video files', '*.mp4 *.flv *.avi *.mkv'), ('All files', '*.*')])
+            if files is not None:
+                self.file_txt.delete(0, END)
+                self.file_txt.insert(0, ';'.join(files))
 
+        file_frame = Frame(self.window)
+        file_frame.pack(fill='both', pady=10)
+        file_lbl = Label(file_frame, text='FILE PATH', width=10)
+        file_lbl.pack(side=LEFT)
+        self.file_txt = Entry(file_frame)
+        self.file_txt.pack(side=LEFT)
+        self.file_btn = Button(file_frame, text='CHOOSE', command=choose_file)
+        self.file_btn.pack(side=LEFT, padx=8)
 
-if __name__ == '__main__':
+        def extract():
+            self._update_status_text()
+            self._running_thread_future = self._thread_pool.submit(
+                self._extract)
 
-    window = Tk()
-    ws = window.winfo_screenwidth()
-    hs = window.winfo_screenheight()
-    w = 300
-    h = 180
-    x = (ws/2) - (w/2)
-    y = (hs/2) - (h/2)
-    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    window.resizable(False, False)
-    window.title('Extract subtitle')
+        self.btn = Button(self.window, text='EXTRACT', command=extract)
+        self.btn.pack()
 
-    model_frame = Frame(window)
-    model_frame.pack(fill='both', pady=10)
-    model_lbl = Label(model_frame, text='MODEL', width=10)
-    model_lbl.pack(side=LEFT)
-    model_combo = Combobox(model_frame, state='readonly')
-    model_combo['values'] = ('tiny.en', 'tiny', 'base.en', 'base', 'small.en',
-                             'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large')
-    model_combo.current(3)
-    model_combo.pack(side=LEFT, ipadx=26)
+        self.status_txt = Text(self.window, height=180)
+        self.status_txt.pack(pady=10, padx=10)
+        self.status_txt.insert(INSERT, 'Click EXTRACT button to start')
 
-    lang_frame = Frame(window)
-    lang_frame.pack(fill='both', pady=10)
-    lang_lbl = Label(lang_frame, text='LANGUAGE', width=10)
-    lang_lbl.pack(side=LEFT)
-    lang_combo = Combobox(lang_frame, state='readonly')
-    lang_combo['values'] = ('en', 'ja', 'zh')
-    lang_combo.current(0)
-    lang_combo.pack(side=LEFT, ipadx=26)
+        self.msg_queue = Text_Queue()
+        sys.stdout = self.msg_queue
 
-    def choose_file():
-        files = filedialog.askopenfilenames(
-            filetypes=[('Video files', '*.mp4 *.flv *.avi *.mkv'), ('All files', '*.*')])
-        if files is not None:
-            file_txt.delete(0, END)
-            file_txt.insert(0, ';'.join(files))
+        self._update_status_text()
 
-    file_frame = Frame(window)
-    file_frame.pack(fill='both', pady=10)
-    file_lbl = Label(file_frame, text='FILE PATH', width=10)
-    file_lbl.pack(side=LEFT)
-    file_txt = Entry(file_frame)
-    file_txt.pack(side=LEFT)
-    file_btn = Button(file_frame, text='CHOOSE', command=choose_file)
-    file_btn.pack(side=LEFT, padx=8)
+        # 关闭时，中止线程池
+        self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-    def clicked():
-        lang = lang_combo.get()
-        inputs = file_txt.get()
-        model_size = model_combo.get()
+    def on_closing(self):
+        print('Closing app...')
+        # 判断是否有正在执行的线程，直接调用cancel
+        if hasattr(self, '_running_thread_future') and not self._running_thread_future.done():
+            cancel_result = self._running_thread_future.cancel()
+            print('Running cancel task result: ', cancel_result)
+            if not cancel_result:
+                sys.exit(1)
+        sys.exit(0)
+
+    def run(self) -> None:
+        self.window.mainloop()
+        self._update_status_text()
+
+    def _update_status_text(self):
+        while not self.msg_queue.empty():
+            content = self.msg_queue.get()
+            self.status_txt.insert(END, content)
+            self.status_txt.see(END)
+            self.status_txt.update()
+
+        self.window.after(100, self._update_status_text)
+
+    def _extract(self) -> None:
+        """
+        处理提取
+        """
+
+        lang = self.lang_combo.get()
+        inputs = self.file_txt.get()
+        model_size = self.model_combo.get()
         if inputs is None or inputs == '':
             messagebox.showerror('Error', 'No file path')
             return
         inputs = inputs.strip(';').split(';')
-        file_btn.config(state='disabled')
-        btn.config(text='Extracting...please wait', state='disabled')
+        self.file_btn.config(state='disabled')
+        self.btn.config(text='Extracting...please wait', state='disabled')
         try:
             for index, input in enumerate(inputs):
-                btn.config(
+                print('Extracting '+input)
+                self.btn.config(
                     text=f'Extracting {index+1}/{len(inputs)}, please wait')
-                btn.update()
-                generate_subtitle(input=input, lang=lang,
-                                  model_size=model_size)
+                self.btn.update()
+                self._generate_subtitle(input=input, lang=lang,
+                                        model_size=model_size)
             messagebox.showinfo('Info', 'Extract successfully!')
         except RuntimeError as err:
             messagebox.showerror('Generate Error', err)
         finally:
-            file_btn.config(state='normal')
-            btn.config(text='EXTRACT', state='normal')
-    btn = Button(window, text='EXTRACT', command=clicked)
-    btn.pack()
+            self.file_btn.config(state='normal')
+            self.btn.config(text='EXTRACT', state='normal')
 
-    window.mainloop()
+    def _generate_subtitle(self, input, lang='en', model_size='base'):
+
+        with open('config.yaml', encoding='utf-8') as f:
+            config = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+        print('input='+input)
+        print('lang='+lang)
+
+        last_dot_index = input.rfind('.')
+        mp3_output = input[:last_dot_index]+'.mp3'
+        srt_output = input[:last_dot_index]+'.srt'
+
+        print('audio extract begin')
+        audio_tool.audio_extract(input, mp3_output)
+        print('audio extract success')
+
+        print('whisper begin')
+        whisper_tool.do_whisper(audio=mp3_output, srt_path=srt_output, language=lang,
+                                model_size=model_size,
+                                model_download_root=config['model_download_root'])
+        print('whisper success')
+
+        print('remove temporary file')
+        os.remove(mp3_output)
+
+        print('success')
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
